b2ae639bf8a241a732fd041c153306a628c08957
SourovDey <developer.sourovdey@gmail.com>
2023-03-30 23:36:53 +0600

    v-2

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-let secondHand = document.querySelector(".second-hand");
-let minHand = document.querySelector(".min-hand");
-let hourHand = document.querySelector(".hour-hand");
+let secondHand = document.querySelector('.second-hand');
+let minsHand = document.querySelector('.min-hand');
+let hourHand = document.querySelector('.hour-hand');
 
-function setDate(){
-    let now = new Date();
-//  ----second------
-    let seconds = now.getSeconds();
-    let secondsDegrees = ((seconds / 60)* 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+ function setDate() {
+  let now = new Date();
 
-    let mins = now.getMinutes();
-    let minsDegrees = ((mins / 60)* 360) +((seconds/60)*6)+ 90;
-    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+  let seconds = now.getSeconds();
+  let secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
+  let mins = now.getMinutes();
+  let minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-    let hour = now.getMinutes();
-    let hoursDegrees = ((hour / 60)* 360) +((mins/60)*30)+ 90;
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  let hour = now.getHours();
+  let hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
 
-setDate()
+setDate();
 
